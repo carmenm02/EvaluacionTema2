812a96e3e74fb0732ad4cbcddddb5de5b6462bc9
carmenm02 <91721886+carmenm02@users.noreply.github.com>
2022-10-07 16:13:26 +0200

    he creado el metodo calificacion y varios alumnos

--- ejercicio1.py
+++ ejercicio1.py
@@ -5,6 +5,17 @@ class Alumno:
     def imprimir(self):
         print("Nombre:",self.nombre)
         print("Nota:",self.nota)
-alumno1 = Alumno("fran",5)
-alumno1.__init__("fran",5)
-alumno1.imprimir()
+    def calificación(self):
+        if self.nota >= 5:
+            print("Aprobado")
+        else:
+            print("Suspenso")
+alumno = Alumno("fran",5)
+alumno.__init__("fran",5)
+alumno.imprimir()
+alumno.calificación()
+
+alumna = Alumno("carmen",7)
+alumna.__init__("carmen",7)
+alumna.imprimir()
+alumna.calificación()
