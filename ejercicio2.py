560e59b9c700ebe63d5f860f738cc55de1239660
carmenm02 <91721886+carmenm02@users.noreply.github.com>
2022-10-07 16:28:48 +0200

    he creado la clase str

---
+++ ejercicio2.py
@@ -0,0 +1,24 @@
+class Alumno:
+    def __init__(self,nombre,nota):
+        self.nombre = nombre
+        self.nota = nota
+    def imprimir(self):
+        print("Nombre:",self.nombre)
+        print("Nota:",self.nota)
+    def __str__(self):
+        
+        return
+    def calificación(self):
+        if self.nota >= 5:
+            print("Aprobado")
+        else:
+            print("Suspenso")
+alumno = Alumno("fran",5)
+alumno.__init__("fran",5)
+alumno.imprimir()
+alumno.calificación()
+
+alumna = Alumno("carmen",7)
+alumna.__init__("carmen",7)
+alumna.imprimir()
+alumna.calificación()
