cf06380cfc25a3d11d32cbae88bbb6e9d24f3bec
carmenm02 <91721886+carmenm02@users.noreply.github.com>
2022-10-08 12:20:56 +0200

    no me funciona el programa

--- ejercicio3.py
+++ ejercicio3.py
@@ -6,3 +6,12 @@ class Producto:
         self.tipo = tipo
     def __str__(self):
         return" El código {} de nombre {} tiene un precio igual a {} y es de tipo {}".format(self.codigo,self.nombre,self.precio,self.tipo)
+    def imprimir(self,producto1):
+        if producto1 == Producto(1,"Iphone",599.9,"smarthphone"):
+            print(producto1)
+        else:
+            print(producto2)
+    
+producto1 = Producto(1,"Iphone",599.9,"smarthphone")
+
+producto2 = Producto(2,"Ipad",699,"tablet")
